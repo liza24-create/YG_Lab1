92768151cc9dfd02238dcb29c7d440d5722c1f9b
YG <lizagya@gmail.com>
2026-09-23 23:13:13 +0300

    Added button, handled onClick and change ViewText content with text

--- app/src/main/java/com/example/yg_lab1/MainActivity.java
+++ app/src/main/java/com/example/yg_lab1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.yg_lab1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClick(View view) {
+        TextView tvMain = findViewById(R.id.tvMain);
+        tvMain.setText("Hello everyone to my first app!");
     }
 }
